bb35deddc88c024d158b28cf8e7a66a4ff595070
mariocodesforfun <gegpriftimario@gmail.com>
2025-10-22 16:00:19 -0400

    refactor bird

--- bird.cpp
+++ bird.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 
+// Abstract base class for all birds
+// This defines what any bird must be able to do
 class Bird {
 public:
-    virtual void fly() {
-        std::cout << "Bird is flying\n";
-    };
-    
-    virtual void lay_egg() {
-        std::cout << "Egg";
-    }
+    virtual void lay_egg() = 0;
 };
 
+// Interface for birds that can fly
+class CanFly {
+public:
+    virtual void fly() = 0;
+};
+
+// Ostrich is a bird, but cannot fly, so it only implements the Bird interface
 class Ostrich : public Bird {
 public:
-    void fly() override {
-        throw std::runtime_error("Ostriches can't fly!");
+    void lay_egg() {
+        std::cout << "Ostrich laid an egg\n";
     }
 };
 
-class Eagle: public Bird {};
+// Eagle is a bird AND can fly, so it implements both interfaces
+class Eagle : public Bird, public CanFly {
+public:
+    void lay_egg() {
+        std::cout << "Eagle laid an egg\n";
+    }
+    
+    void fly() {
+        std::cout << "Eagle is flying\n";
+    }
+};
 
 // For demonstration
 int main() {
     Eagle eagle;
     Ostrich ostrich;
     
+    // Both can lay eggs
     eagle.lay_egg();
-    eagle.fly();
-
     ostrich.lay_egg();
-    ostrich.fly();
+    
+    // Only eagle can fly
+    eagle.fly();
+      
+    return 0;
 }
