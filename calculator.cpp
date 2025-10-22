e146e922ec85b5905c12e2d382cf57563abf237f
mariocodesforfun <gegpriftimario@gmail.com>
2025-10-22 16:12:54 -0400

    refactor calculator to follow SOLID

--- calculator.cpp
+++ calculator.cpp
@@ -1,25 +1,55 @@
-#include <string>
 #include <iostream>
 
+// defines what any calculators must be able to do
 class DiscountCalculator {
 public:
-    double calculate(const std::string& customerType, double amount) {
-        if (customerType == "Loyal") {
-            return amount * 0.95;
-        } else if (customerType == "VIP") {
-            return amount * 0.90;
-        } else {
-            return amount;
-        }
+    virtual double calculate(double amount) = 0;
+};
+
+// only calculates loyal customer discounts (5% off)
+class LoyalCustomerDiscountCalculator : public DiscountCalculator {
+public:
+    double calculate(double amount) {
+        return amount * 0.95;
     }
 };
 
-// Included for demonstration
+// only calculates VIP customer discounts (10% off)
+class VIPCustomerDiscountCalculator : public DiscountCalculator {
+public:
+    double calculate(double amount) {
+        return amount * 0.90;
+    }
+};
+
+// Handles regular customers without any discount
+class OtherCustomerDiscountCalculator : public DiscountCalculator {
+public:
+    double calculate(double amount) {
+        return amount;
+    }
+};
+
+// usage of the discount calculator
+class PriceCalculator {
+private:
+    DiscountCalculator& calculator;
+public:
+    PriceCalculator(DiscountCalculator& calc) : calculator(calc) {}
+    
+    double calculateFinalPrice(double price) {
+        return calculator.calculate(price);
+    }
+};
+
 int main() {
-    DiscountCalculator calc;
     double price = 1000;
 
-    std::cout << "VIP Customer Price: " << calc.calculate("VIP", price) << "\n";
-    std::cout << "Loyal Customer Price: " << calc.calculate("Loyal", price) << "\n";
-    std::cout << "Other Customer Price: " << calc.calculate("Other", price) << "\n";
+    VIPCustomerDiscountCalculator vip;
+    LoyalCustomerDiscountCalculator loyal;
+    OtherCustomerDiscountCalculator other;
+
+    std::cout << "VIP Customer Price: " << PriceCalculator(vip).calculateFinalPrice(price) << "\n";
+    std::cout << "Loyal Customer Price: " << PriceCalculator(loyal).calculateFinalPrice(price) << "\n";
+    std::cout << "Other Customer Price: " << PriceCalculator(other).calculateFinalPrice(price) << "\n";
 }
